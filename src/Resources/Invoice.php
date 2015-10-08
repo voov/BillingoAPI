ef34e052200089af40ecfcf1a59dacf3012bd7c1
Daniel Fekete <daniel.fekete@voov.hu>
2015-10-08 12:17:31 +0200

    Invoice init

---
+++ src/Resources/Invoice.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright (c) 2015, VOOV LLC.
+ * All rights reserved.
+ * Written by Daniel Fekete
+ */
+
+namespace Billingo\API\Client\Resources;
+
+
+class Invoice implements ResourceInterface
+{
+
+	/**
+	 * Return the list of resources
+	 * @return mixed
+	 */
+	public function index()
+	{
+		// TODO: Implement index() method.
+	}
+
+	/**
+	 * Read one resource
+	 * @param $id
+	 * @return mixed
+	 */
+	public function read($id)
+	{
+		// TODO: Implement read() method.
+	}
+
+	/**
+	 * Create a new resource
+	 * @param $data
+	 * @return mixed
+	 */
+	public function create($data)
+	{
+		// TODO: Implement create() method.
+	}
+
+	/**
+	 * Update resource
+	 * @param $id
+	 * @param $data
+	 * @return mixed
+	 */
+	public function update($id, $data)
+	{
+		// TODO: Implement update() method.
+	}
+
+	/**
+	 * Delete resource
+	 * @param $id
+	 * @return mixed
+	 */
+	public function delete($id)
+	{
+		// TODO: Implement delete() method.
+	}
+}
